8747df61f867ed47b42a5ec4cfa6217a58fef91e
markyupari <yupari87157@gmail.com>
2023-05-28 15:23:33 -0500

    test modified

--- hello.js
+++ hello.js
@@ -1,3 +1,5 @@
-function hello() {
+const hello = () => {
   return "Hello World!";
 }
+
+module.exports = hello;
--- hello.test.js
+++ hello.test.js
@@ -1,5 +1,6 @@
-var utils  = require('course-utilities');
-var hello = utils.load('./hello.js', 'hello');
+// var utils  = require('course-utilities');
+// var hello = utils.load('./hello.js', 'hello');
+var hello = require('./hello');
 
 test('outputs the correct string', () => {
   expect(hello()).toBe("Hello World!");
